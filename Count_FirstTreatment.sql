5b0303b6d049414e371f3bf5f56ff08e60d7618e
IAPT GitHub <98588750+NHSE-IAPT@users.noreply.github.com>
2022-01-31 12:10:21 +0000

    initial upload of a standard set of SQL queries

---
+++ Count_FirstTreatment.sql
@@ -0,0 +1,33 @@
+
+-- This query will return the total number of initial referrals acessing IAPT, as displayed on the NHS Digital website 'lastest statistics' publication ---------------
+
+--- This file is designed to accompany section [] of the IAPT beginners guide (2.1)
+
+
+---- start of script --------------------------------------------------------------------------------------------------------------------------------------------------
+
+--- declare variables to hold values for PeriodStart and PeriodEnd dates
+
+DECLARE @PeriodStart DATE
+DECLARE @PeriodEnd DATE
+
+--- set the values for PeriodStart and PeriodEnd dates
+
+SET @PeriodStart = '2021-09-01'
+SET @PeriodEnd = '2021-09-30'
+
+-- SELECT statement
+
+SELECT  COUNT(DISTINCT CASE WHEN TherapySession_FirstDate BETWEEN @PeriodStart and @PeriodEnd THEN PathwayID ELSE NULL END) AS Count_FirstTreatment
+
+-- FROM statement
+
+FROM [NHSE_IAPT_v2].dbo.IDS101_Referral r
+INNER JOIN [NHSE_IAPT_v2].[dbo].[IsLatest_SubmissionID] l ON r.[UniqueSubmissionID] = l.[UniqueSubmissionID] AND r.AuditId = l.AuditId
+INNER JOIN [NHSE_IAPT_v2].[dbo].[IDS000_Header] h ON r.[UniqueSubmissionID] = h.[UniqueSubmissionID]
+
+-- WHERE statement
+
+WHERE UsePathway_Flag = 'True' AND IsLatest = 1 AND h.[ReportingPeriodStartDate] BETWEEN DATEADD(MONTH, 0, @PeriodStart) AND @PeriodStart
+
+---- end of script ----------------------------------------------------------------------------------------------------------------------------------------------------
---
+++ Count_SelfReferrals.sql
@@ -0,0 +1,33 @@
+
+-- This query will return the total number of self referrals for IAPT treatment, as displayed on the NHS Digital website 'lastest statistics' publication ----------------------------
+
+--- This file is designed to accompany section [] of the IAPT beginners guide (2.1)
+
+
+---- start of script -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--- declare variables to hold values for PeriodStart and PeriodEnd dates
+
+DECLARE @PeriodStart DATE
+DECLARE @PeriodEnd DATE
+
+--- set the values for PeriodStart and PeriodEnd dates
+
+SET @PeriodStart = '2021-09-01'
+SET @PeriodEnd = '2021-09-30'
+
+-- SELECT statement
+
+SELECT  COUNT(DISTINCT CASE WHEN ReferralRequestReceivedDate BETWEEN @PeriodStart and @PeriodEnd AND SourceOfReferralMH = 'B1' THEN PathwayID ELSE NULL END) AS Count_SelfReferrals
+
+-- FROM statement
+
+FROM [NHSE_IAPT_v2].dbo.IDS101_Referral r
+INNER JOIN [NHSE_IAPT_v2].[dbo].[IsLatest_SubmissionID] l ON r.[UniqueSubmissionID] = l.[UniqueSubmissionID] AND r.AuditId = l.AuditId
+INNER JOIN [NHSE_IAPT_v2].[dbo].[IDS000_Header] h ON r.[UniqueSubmissionID] = h.[UniqueSubmissionID]
+
+-- WHERE statement
+
+WHERE UsePathway_Flag = 'True' AND IsLatest = 1 AND h.[ReportingPeriodStartDate] BETWEEN DATEADD(MONTH, 0, @PeriodStart) AND @PeriodStart
+
+---- end of script -------------------------------------------------------------------------------------------------------------------------------------------------------------------
