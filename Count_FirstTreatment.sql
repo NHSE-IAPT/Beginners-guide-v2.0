823b45844619935c83a6db5014368e31940451ee
IAPT GitHub <98588750+NHSE-IAPT@users.noreply.github.com>
2022-02-02 15:23:50 +0000

    Update Count_FirstTreatment.sql

--- Count_FirstTreatment.sql
+++ Count_FirstTreatment.sql
@@ -1,4 +1,3 @@
-
 -- This script is designed to accompany the 'Replicating published data' section of the IAPT beginners guide (2.1)
 
 -- The query will return the total number of initial referrals acessing IAPT, as displayed on the NHS Digital website 'lastest statistics' publication --------
@@ -13,19 +12,15 @@ DECLARE @PeriodEnd DATE
 SET @PeriodStart = 'yyyy-mm-dd' -- overwrite placeholder with required month start date
 SET @PeriodEnd = 'yyyy-mm-dd' -- overwrite placeholder with required month end date
 
-
 -- SELECT statement
-
 SELECT  COUNT(DISTINCT CASE WHEN TherapySession_FirstDate BETWEEN @PeriodStart and @PeriodEnd THEN PathwayID ELSE NULL END) AS Count_FirstTreatment
 
 -- FROM statement
-
 FROM [NHSE_IAPT_v2].dbo.IDS101_Referral r
 INNER JOIN [NHSE_IAPT_v2].[dbo].[IsLatest_SubmissionID] l ON r.[UniqueSubmissionID] = l.[UniqueSubmissionID] AND r.AuditId = l.AuditId
 INNER JOIN [NHSE_IAPT_v2].[dbo].[IDS000_Header] h ON r.[UniqueSubmissionID] = h.[UniqueSubmissionID]
 
 -- WHERE statement
-
 WHERE UsePathway_Flag = 'True' AND IsLatest = 1 AND h.[ReportingPeriodStartDate] BETWEEN DATEADD(MONTH, 0, @PeriodStart) AND @PeriodStart
 
 ---- end of script ----------------------------------------------------------------------------------------------------------------------------------------------------
